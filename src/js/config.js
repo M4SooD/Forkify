4e8d6efb0c5b945b0f388952039f2c61b8d6fbc5
masood <masooodm@yahoo.com>
2022-01-18 12:45:11 +0330

    commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = 'df7c0514-5b46-457f-ab19-c18fe6d4dc44';
+export const MODAL_CLOSE_SEC = 2.5,
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,14 @@
+import View from './view';
+import previewView from './previewView.js';
+import icons from '../../img/icons.svg';
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your search! Please try again!';
+  _successMessage = '';
+
+  _generateMarkup() {
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+}
+
+export default new ResultsView();
